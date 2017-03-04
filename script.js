6742a6f597d977aba359c48427eb2de8455ff218
sheebat <sheeba.thukral@gmail.com>
2017-03-03 16:28:40 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Sheeba on 3/1/2017.
+ */
+
+var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var employees = [
+            { name: "Ben", gender: "Male", salary: 55000 },
+            { name: "Sara", gender: "Female", salary: 68000 },
+            { name: "Mark", gender: "Male", salary: 57000 },
+            { name: "Pam", gender: "Female", salary: 53000 },
+            { name: "Todd", gender: "Male", salary: 60000 }
+        ];
+
+        $scope.employees = employees;
+        $scope.employeeList = "EmployeeList.html";
+    });
